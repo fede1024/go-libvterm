f644ad0fef8509c94339c88bcd66df97870c2740
Federico Giraud <giraud.federico@gmail.com>
2015-08-22 17:13:29 +0100

    Printing

--- main.go
+++ main.go
@@ -55,11 +55,20 @@ func moveCursor(pos C.VTermPos, oldPos C.VTermPos, visible C.int, user unsafe.Po
 	return 0
 }
 
+//func cellToStringI
+
 func printRect(screen *C.VTermScreen, rect TermRect) {
 	cell := C.VTermScreenCell{}
 	pos := C.VTermPos{}
+	fmt.Printf("+")
+	for c := rect.start_col; c < rect.end_col; c++ {
+		fmt.Printf("-")
+	}
+	fmt.Println("+")
+
 	for r := rect.start_row; r < rect.end_row; r++ {
-		for c := rect.start_col; c < rect.end_col; c++ {
+		fmt.Printf("|")
+		for c := rect.start_col; c < rect.end_col; {
 			pos.col = C.int(c)
 			pos.row = C.int(r)
 			C.vterm_screen_get_cell(screen, pos, &cell)
@@ -67,14 +76,22 @@ func printRect(screen *C.VTermScreen, rect TermRect) {
 			var buf bytes.Buffer
 			for i := 0; i < C.VTERM_MAX_CHARS_PER_CELL && cell.chars[i] != 0; i++ {
 				buf.WriteRune((rune)(cell.chars[i]))
-				fmt.Printf("> %x\n", cell.chars[i])
 			}
-			fmt.Printf("-- %s\n", buf.String())
-			//fmt.Printf("> %d  %s\n", cell.width, string(cell.chars))
-			//fmt.Printf("%s", string(cell.chars[0]))
+			c += int(cell.width)
+			if buf.Len() == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print(buf.String())
+			}
 		}
-		fmt.Println("")
+		fmt.Println("|")
 	}
+
+	fmt.Printf("+")
+	for c := rect.start_col; c < rect.end_col; c++ {
+		fmt.Printf("-")
+	}
+	fmt.Println("+")
 }
 
 func main() {
@@ -99,7 +116,7 @@ func main() {
 			select {
 			case rect := <-term.damage:
 				fmt.Printf("damage: %d\n", rect)
-				printRect(screen, rect)
+				//printRect(screen, rect)
 			case pos := <-term.cursorMove:
 				fmt.Printf("move cursor: %d\n", pos)
 			case <-term.stop:
@@ -111,7 +128,6 @@ func main() {
 
 	go func() {
 		for str := range term.inputStr {
-			fmt.Printf(">>> %d\n", len(str))
 			C.vterm_input_write(vt, C.CString(str), (C.size_t)(len(str)))
 		}
 	}()
@@ -120,10 +136,12 @@ func main() {
 	fmt.Println("\xC3\x81\xC3\xA9")
 	fmt.Println("\u2603")
 	//term.inputStr <- "\xC3\x81\xC3\xA9"
-	//term.inputStr <- "\xC3\x81\xC3\xA9"
+	term.inputStr <- "\xC3\x81\xC3\xA9"
 	//term.inputStr <- "\x1b[H"
-	term.inputStr <- "e\xCC\x81"
-	term.inputStr <- "e\xCC\x81\xCC\x82\xCC\x83\xCC\x84\xCC\x85\xCC\x86\xCC\x87\xCC\x88\xCC\x89\xCC\x8A"
+	term.inputStr <- "p\xCC\x81"
+	term.inputStr <- "a\xCC\x8A"
+	term.inputStr <- "zero\xEF\xBC\x90lol"
+	term.inputStr <- "\n\rzero\xEF\xBC\x90lol"
 
 	C.vterm_keyboard_unichar(vt, '\u2603', 0)
 	bufLen := C.vterm_output_get_buffer_current(vt)
@@ -134,14 +152,9 @@ func main() {
 
 	time.Sleep(100 * time.Millisecond)
 
-	rect := C.VTermRect{0, 0, 5, 5}
-	charLen := C.vterm_screen_get_chars(screen, nil, 0, rect)
-
-	fmt.Printf("> %d", charLen)
-
 	fmt.Println("")
-	//rect2 := TermRect{0, 0, 5, 10}
-	//printRect(screen, rect2)
+	rect2 := TermRect{0, 0, 5, 20}
+	printRect(screen, rect2)
 
 	fmt.Printf("DONE\n")
 }
